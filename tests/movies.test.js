cc927da6ca24c4eb0672878f5beb9cc5d60bdaf8
Meriem Chikha <meriem.chikha@laposte.net>
2024-01-28 21:23:37 +0100

    movies-test

---
+++ tests/movies.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/movies/:id", () => {
+  it("should return a movie which id is 1", async () => {
+    const response = await request(app).get("/api/movies/1");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/movies/:id", () => {
+  it("should return a movie which id is 0", async () => {
+    const response = await request(app).get("/api/movies/0");
+
+    expect(response.status).toEqual(404);
+  });
+});
